e83bb8e5d88d9997e3401933d80819809b50fe54
imrannavriti <imran.k@navriti.com>
2020-11-25 13:13:10 +0530

    Initial commit

---
+++ app/src/main/java/com/youz/uznetworking/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.youz.uznetworking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.youz.networkinglibrary.MyMath
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ networkinglibrary/src/main/java/com/youz/networkinglibrary/MyMath.kt
@@ -0,0 +1,10 @@
+package com.youz.networkinglibrary
+
+class MyMath {
+
+    companion object {
+        fun add( a: Int, b: Int): Int {
+            return (a + b);
+        }
+    }
+}
